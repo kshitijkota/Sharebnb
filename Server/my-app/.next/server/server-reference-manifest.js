8d5da7be266271b018da83bbfd34a10a6efd4a55
Kshitij <kshitijkota22@gmail.com>
2025-01-12 16:51:43 +0530

    Idk

---
+++ Server/my-app/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"KmDeHI0j0Hf950lnv1c8cpF93IU6KhwErDJKdO9KuOY=\"\n}"
---
+++ Server/my-app/.next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"__rewrites":{"afterFiles":[],"beforeFiles":[],"fallback":[]},"/_app":["static/chunks/pages/_app.js"],"/_error":["static/chunks/pages/_error.js"],"/api/storeData":["static/chunks/pages/api/storeData.js"],"sortedPages":["/_app","/_error","/api/storeData"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
