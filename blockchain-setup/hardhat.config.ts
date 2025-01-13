ca52ece0763be0eed7adc601da104d858017aa85
Pranavjeet-Naidu <pranavjeetnaidu@gmail.com>
2025-01-13 22:45:19 +0530

    fuji :)

--- blockchain-setup/hardhat.config.ts
+++ blockchain-setup/hardhat.config.ts
@@ -17,8 +17,13 @@ const config: HardhatUserConfig = {
     },
     mumbai: {
       url: "https://rpc-amoy.polygon.technology/", // Use an RPC URL for Polygon Mumbai (e.g., from MaticVigil, Alchemy, or Infura)
-      accounts: [process.env.PRIVATE_KEY || ""], // Add your private key (use env variable for security)
+      accounts: ["d87de29009328f4ec7b9cdbb3c9084fb0e75c2c8e04210dad8e7c1d70e7ea62c"], // Add your private key (use env variable for security)
       chainId: 80001, // Mumbai Testnet Chain ID
+    },
+    fuji: {
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      accounts: ["1464cfd85c09e9582a0ce08caa5c561f6d66a81743dc432bd6d82c77c0f684d5"], 
+      chainId: 43113,// Use your wallet's private key
     },
   },
 };
